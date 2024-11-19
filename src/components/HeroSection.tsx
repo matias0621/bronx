431cdd6b0b9e57f3df1447eb8db13908e8ffdd88
matias0621 <132005393+matias0621@users.noreply.github.com>
2024-11-19 13:11:12 -0300

    Obvio que no termino por supuesto que no

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -4,12 +4,12 @@ export default function HeroSection() {
   return (
     <>
       <section className="background-overlay text-white">
-        <div className="relative text-white text-center p-5 z-10 lg:pt-16 lg:pl-28">
-          <h1 className="text-5xl mb-4 sm:text-start sm:text-3xl sm:w-[80%] lg:w-[40%] lg:text-[1.8rem] ">
+        <div className="relative flex flex-col justify-center items-center h-full gap-y-8 lg:block text-white text-center p-5 z-10 lg:pt-16 lg:pl-28">
+          <h1 className="text-5xl mb-4 sm:text-start sm:text-5xl sm:w-[80%] lg:w-[60%] lg:text-[1.8rem] ">
             The team THE CAR is located directly in Germany, in the city of
             Berlin.
           </h1>
-          <p className="sm:text-start sm:text-xs sm:w-[70%] sm:mt-8 lg:w-[30%]">
+          <p className="text-xl sm:text-start sm:text-xl sm:w-[70%] lg:text-[1rem] sm:mt-8 lg:w-[50%]">
             This allows us to swiftly process your orders and locate the best
             cars in the shortest possible time. We simplify the complex process
             of buying a car in Germany to make it as convenient as possible for
